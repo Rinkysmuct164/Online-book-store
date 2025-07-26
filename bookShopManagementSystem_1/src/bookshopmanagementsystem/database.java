ab6d27f078e4da4cd72285f40b2062bbbd99eefc
Rinkysmuct164 <badonsikderrinky@gmail.com>
2025-07-26 11:17:07 +0600

    Add files via upload

---
+++ bookShopManagementSystem_1/src/bookshopmanagementsystem/database.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bookshopmanagementsystem;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @Author     : MarcoMan
+ */
+public class database {
+    
+    public static Connection connectDb(){
+        
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connect = DriverManager.getConnection("jdbc:mysql://localhost/book1", "root", ""); // address, database username, database password
+            return connect;
+        }catch(Exception e){e.printStackTrace();}
+        return null; // LETS MAKE OUR DATABASE  : ) book is our database name : ) 
+    }
+    
+}
